befa1d7cf1ee08bda2dc3c3f2ca6548cc06305cb
Pavel Zvonnikov <pavelzvonnikov@yandex.ru>
2020-03-19 22:07:16 +0300

    add changes

--- src/HOC/FetchPostsHOC.jsx
+++ src/HOC/FetchPostsHOC.jsx
@@ -1,26 +1,78 @@
 import React from "react";
 
+const ImgAvatar1 = require("../images/cat1.jpg");
+const ImgAvatar2 = require("../images/cat2.jpg");
+const ImgAvatar3 = require("../images/cat3.jpg");
+const ImgAvatar4 = require("../images/notcat.jpg");
+
 export const FetchPostsHOC = Component =>
   class FetchPostsHOC extends React.Component {
     constructor() {
       super();
 
       this.state = {
-        posts: []
+        posts: [],
+        avatars: {
+          1: ImgAvatar1,
+          2: ImgAvatar2,
+          3: ImgAvatar3,
+          4: ImgAvatar4
+        },
+        descriptions: [
+          "Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat.",
+          "To travelling occasional at oh sympathize prosperous.",
+          "Of distrusts immediate enjoyment curiosity do.",
+          "At as do be against pasture covered viewing started.Enjoyed me settled mr respect no spirits civilly.",
+          "Collected breakfast estimable questions in to favourite it. Known he place worth words it as to.",
+          "Celebrated if remarkably especially an.",
+          "May indulgence difficulty ham can put especially.",
+          "Perhaps far exposed age effects.",
+          "Sang put paid away joy into six her."
+        ]
       };
     }
 
+    randomInteger = (min, max) => {
+      let rand = min - 0.5 + Math.random() * (max - min + 1);
+      return Math.round(rand);
+    };
+
+    addDescription = arr => {
+      const { descriptions } = this.state;
+      let newArr = [];
+      arr.forEach(item => {
+        let random = this.randomInteger(0, 8);
+        newArr.push({ ...item, description: descriptions[random] });
+      });
+      return newArr;
+    };
+
+    addAvatar = arr => {
+      const { avatars } = this.state;
+      let newArr = [];
+      arr.forEach(item => {
+        let random = this.randomInteger(1, 4);
+        newArr.push({ ...item, avatar: avatars[random] });
+      });
+      return newArr;
+    };
+
     changeCount = (id, like) => {
       const { posts } = this.state;
-      const list = [...posts];
-      console.log("before", list);
+      let list = [];
+      list = posts.reduce((arr, current) => {
+        const elemOfObj = { ...current };
+        arr.push(elemOfObj);
+        return arr;
+      }, []);
+
       let index = list.findIndex(item => item.id === id);
       if (!like) {
         list[index].likes++;
       } else {
         list[index].likes--;
       }
-      console.log("after", list);
+
       this.setState({
         posts: list
       });
@@ -35,8 +87,7 @@ export const FetchPostsHOC = Component =>
     };
     randomPostTime = arr => {
       for (let i = 0; i < arr.length; i++) {
-        let rand = 1 - 0.5 + Math.random() * 23;
-        let time = Math.round(rand);
+        let time = this.randomInteger(1, 23);
         arr[i].time = time;
       }
       return arr;
@@ -44,54 +95,24 @@ export const FetchPostsHOC = Component =>
 
     randomLikesCount = arr => {
       for (let i = 0; i < arr.length; i++) {
-        let rand = 10 - 0.5 + Math.random() * (1000 - 9);
-        let likes = Math.round(rand);
+        let likes = this.randomInteger(10, 1200);
         arr[i].likes = likes;
       }
       return arr;
     };
 
-    // getPosts = () => {
-    //   axios({
-    //     method: "get",
-    //     url: "https://api.flickr.com/services/rest",
-    //     params: {
-    //       method: "flickr.favorites.getList",
-    //       api_key: API_KEY,
-    //       user_id: "187413433@N05",
-    //       extras: "url_n, owner_name, date_taken, views",
-    //       page: 1,
-    //       format: "json",
-    //       nojsoncallback: 1,
-    //       per_page: 2
-    //     }
-    //   })
-    //     .then(response => {
-    //       console.log("res", response.data.photos.photo);
-    //       const posts = this.transformDate(response.data.photos.photo);
-    //       this.setState({
-    //         posts
-    //       });
-    //     })
-    //     .catch(error => console.log(error.response));
-    // };
     getPosts = () => {
       fetch("https://picsum.photos/v2/list?page=1&limit=9")
         .then(response => response.json())
         .then(data => {
-          const posts = this.transformName(data);
-          return posts;
-        })
-        .then(data => {
-          const posts = this.randomPostTime(data);
-          return posts;
-        })
-        .then(data => {
-          const posts = this.randomLikesCount(data);
+          let posts = this.transformName(data);
+          posts = this.randomPostTime(posts);
+          posts = this.randomLikesCount(posts);
+          posts = this.addAvatar(posts);
+          posts = this.addDescription(posts);
           return posts;
         })
         .then(posts => {
-          console.log(posts);
           this.setState({
             posts
           });
