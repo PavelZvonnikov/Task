4b09052eb107ff9a510612a221c1a364e82cad0e
Pavel Zvonnikov <pavelzvonnikov@yandex.ru>
2020-03-19 23:14:23 +0300

    fix styles in post

---
+++ build/precache-manifest.8671812e676510206bfb17b9d25a6565.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26fdaf3131b67b88451547b81d70cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8bcb656a655da4f675a",
+    "url": "/static/css/main.7377d747.chunk.css"
+  },
+  {
+    "revision": "ad4ef9a60875cce7170b",
+    "url": "/static/js/2.ddd71a1e.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.ddd71a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8bcb656a655da4f675a",
+    "url": "/static/js/main.8871218d.chunk.js"
+  },
+  {
+    "revision": "b04d2da1433e43d10a70",
+    "url": "/static/js/runtime-main.28094b3c.js"
+  },
+  {
+    "revision": "009ada7c6d304bd7e978b63ea4b5f723",
+    "url": "/static/media/cat1.009ada7c.jpg"
+  },
+  {
+    "revision": "cf268a00aa3bfb8dc9823df3c709e0f8",
+    "url": "/static/media/cat2.cf268a00.jpg"
+  }
+]);
